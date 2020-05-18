2faf47e94115c9d1b3f4ff617a7a509418f2202b
genius-gil <worlfq@nate.com>
2020-05-19 03:07:32 +0900

    gugu.py 삭제

--- gugu.py
+++
@@ -1,7 +0,0 @@
-print('Hello world!!, This is a gugudan program2\n')
-
-for i in range(2, 10):
-    print('[======2dans======]')
-    for j in range(1, 10):
-        print('{0}*{1}={2}'.format(i, j, i*j))
-    print('\n')
